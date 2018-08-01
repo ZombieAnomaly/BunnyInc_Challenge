d2dae28cdefcb77ff2e68fe4f94bfa9b5124f3df
ZombieAnomaly <kehran3@gmail.com>
2018-08-01 15:44:54 -0700

    WIP

--- APIs/PostTweet.php
+++ APIs/PostTweet.php
@@ -3,10 +3,11 @@ require 'TwitterAPISettings.php';
 
 $post = $_POST['data'];
 $content = $post['content'];
-if (strpos($content, '#NowPlaying') !== false) {
-    $content = $content . ' #NowPlaying';
+$hashtag = "";
+if (strpos($content, '#NowPlaying') == false) {
+    $hashtag = '#NowPlaying ';
 }
-$status = $connection->post("statuses/update", ["status" => $content . ' ' . $post['url'] ]);
+$status = $connection->post("statuses/update", ["status" => $hashtag . $content . ' ' . $post['url'] ]);
 echo json_encode($status);
 
 //echo json_encode($post);
