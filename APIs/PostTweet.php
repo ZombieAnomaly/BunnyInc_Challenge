367e36c7a86f5fc124f46c5a24428f421d88ce64
ZombieAnomaly <kehran3@gmail.com>
2018-08-01 15:38:09 -0700

    fixed PostAPI to append #NowPlaying if not already present in status

--- APIs/PostTweet.php
+++ APIs/PostTweet.php
@@ -2,8 +2,11 @@
 require 'TwitterAPISettings.php';
 
 $post = $_POST['data'];
-
-$status = $connection->post("statuses/update", ["status" => $post['content'] . ' ' . $post['url'] ]);
+$content = $post['content'];
+if (strpos($content, '#NowPlaying') !== false) {
+    $content = $content . ' #NowPlaying';
+}
+$status = $connection->post("statuses/update", ["status" => $content . ' ' . $post['url'] ]);
 echo json_encode($status);
 
 //echo json_encode($post);
